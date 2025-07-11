c9cf26d511d460ed22b0a2e4adc4e92e03e68894
Aman <amankumarsingh.org@gmail.com>
2025-07-11 14:54:20 +0530

    Changed Main Homepage Header

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,28 +1,26 @@
-'use client'
+'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './navbar.css';
-import Link from "next/link";
+import Link from 'next/link';
 
-const AmazonNavigationBar = () => {
+const WalmartNavigationBar = () => {
   return (
-    <div className="amazon-nav">
-      <div className="amazon-nav-section">
-        <ul className="amazon-nav-list">
-          <li><a href="#">All Departments</a></li>
-          <li><a href="#">Grocery & Essentials</a></li>
-          <li><a href="#">Fashion</a></li>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Electronics</a></li>
-          <li><a href="#">Toys</a></li>
-          <li><a href="#">Sports & Outdoors</a></li>
-          <li><a href="#">Auto & Tires</a></li>
-          <li><a href="#">Health & Wellness</a></li>
-          <li><a href="#">Pharmacy</a></li>
-        </ul>
-      </div>
-    </div>
+    <nav className="navblue">
+      <ul className="navblue__list">
+        <li><a href="#">All Departments</a></li>
+        <li><a href="#">Grocery & Essentials</a></li>
+        <li><a href="#">Fashion</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Electronics</a></li>
+        <li><a href="#">Toys</a></li>
+        <li><a href="#">Sports & Outdoors</a></li>
+        <li><a href="#">Auto & Tires</a></li>
+        <li><a href="#">Health & Wellness</a></li>
+        <li><a href="#">Pharmacy</a></li>
+      </ul>
+    </nav>
   );
 };
 
-export default AmazonNavigationBar;
+export default WalmartNavigationBar;
